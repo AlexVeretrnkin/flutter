c3ec849b7f7e2dc5cf27552452816fa810bb5fa7
AlexVeretenkin <alexveretenkin@gmail.com>
2020-10-29 00:23:45 +0200

    Added provider

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,7 @@ import 'pages/main.dart';
 import 'pages/menu.dart';
 import 'pages/selection-screen.dart';
 
-const Map<int, String> Routes = {
-  0: '/',
-  1: '/second',
-  2: '/third',
-  3: '/menu'
-};
+const Map<int, String> Routes = {0: '/', 1: '/second', 2: '/third', 3: '/menu'};
 
 List<PreviewItem> sectionData = [
   PreviewItem.image(
@@ -39,16 +34,23 @@ List<PreviewItem> sectionVideoData = [
 void main() {
   // runApp(MyApp()); - sample app (first created)
 
-  runApp(
-      ChangeNotifierProvider(
-        create: (context) => PreviewItemProvider(),
-        child: Main(),
-      )
-  );
+  runApp(ChangeNotifierProvider(
+    create: (context) => PreviewItemProvider(),
+    child: Main(),
+  ));
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
+  Main({Key key}) : super(key: key);
+
+  @override
+  _Main createState() => _Main();
+}
+
+class _Main extends State<Main> {
   final _navigatorKey = GlobalKey<NavigatorState>();
+
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -146,10 +148,23 @@ class Main extends StatelessWidget {
         ),
         bottomNavigationBar: BottomNavigationBar(
             backgroundColor: Color.fromARGB(255, 50, 42, 38),
+            currentIndex: _selectedIndex,
             selectedItemColor: Colors.white,
             unselectedItemColor: Colors.grey,
             onTap: (int index) {
-              _navigatorKey.currentState.pushNamed(Routes[index]);
+              if (_selectedIndex != index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+
+                _navigatorKey.currentState.pushReplacementNamed(Routes[index]);
+
+                // if (index == 0 && _navigatorKey.currentState.canPop()) {
+                //   _navigatorKey.currentState.pop();
+                // } else {
+                //   // _navigatorKey.currentState.pushNamedAndRemoveUntil(Routes[index], ModalRoute.withName('/'));
+                // }
+              }
             },
             items: [
               BottomNavigationBarItem(
@@ -159,11 +174,8 @@ class Main extends StatelessWidget {
                 title: Text('Explore'),
               ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.library_music),
-                  title: Text('Library')
-              ),
-            ]
-        ),
+                  icon: Icon(Icons.library_music), title: Text('Library')),
+            ]),
       ),
     );
   }
